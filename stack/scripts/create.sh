1bbd85768e5ae62cc0324e6c1b1567471099f42c
Tatum Szymczak <tatum@ashlandstudios.com>
2016-09-10 09:08:01 -0500

    change cloudormation template to JSON instead of YAML

--- stack/scripts/create.sh
+++ stack/scripts/create.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 stack=www-dietsmarts-info
-body=$(ruby -ryaml -rjson -e "puts YAML.load_file('stack/cloud.yaml').to_json")
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 aws cloudformation create-stack \
   --stack-name $stack \
-  --template-body "${body}" \
+  --template-body file://$DIR/../cloud.json \
   --capabilities CAPABILITY_IAM && \
 
   aws cloudformation wait stack-create-complete \
